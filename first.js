d607f2e5aa901d697db82144f954f34dbdd588b1
probablyvivek <viv.tiwari13@gmail.com>
2022-07-20 22:37:32 +0530

    First Javascript File

--- first.js
+++ first.js
@@ -1,1 +1,7 @@
-console.log("Subscribe Now");
+const prompt = require('prompt-sync')();
+
+var name = prompt('What is your name? ');
+console.log('Hello ' + name);
+
+// Language: javascript
+// Path: Learn/first.jsV
